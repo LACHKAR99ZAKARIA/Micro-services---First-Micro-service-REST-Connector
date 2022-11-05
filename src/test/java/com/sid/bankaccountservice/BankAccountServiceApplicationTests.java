e8a3fa6d2bb9002a53fac70eee43ee348cf3a8da
unknown <LACHKAR_ZAKARIA@emsi-edu.ma>
2022-11-05 17:25:50 +0100

    c

---
+++ src/test/java/com/sid/bankaccountservice/BankAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.bankaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
